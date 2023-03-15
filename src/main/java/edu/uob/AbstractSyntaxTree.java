d226ceb6f419fd3284894107d1b5b7774e7510e9
Alex Cockrean <alexander.cockrean@gmail.com>
2023-03-15 14:00:12 +0000

    Making enum toString

--- src/main/java/edu/uob/AbstractSyntaxTree.java
+++ src/main/java/edu/uob/AbstractSyntaxTree.java
@@ -15,4 +15,28 @@ public class AbstractSyntaxTree {
         fsm.populateTree();
     }
 
+    @Override
+    public String toString() {
+        return toString(root);
+    }
+
+    private String toString(Node rt) {
+        StringBuilder buffer = new StringBuilder(500);
+        print(buffer, "", "", rt);
+        return buffer.toString();
+    }
+
+    private void print(StringBuilder buffer, String prefix, String childrenPrefix, Node rt) {
+        buffer.append(prefix);
+        buffer.append(rt.getType().toString());
+        buffer.append('\n');
+        for (Iterator<Node> it = children.iterator(); it.hasNext();) {
+            TreeNode next = it.next();
+            if (it.hasNext()) {
+                next.print(buffer, childrenPrefix + "├── ", childrenPrefix + "│   ");
+            } else {
+                next.print(buffer, childrenPrefix + "└── ", childrenPrefix + "    ");
+            }
+        }
+    }
 }
