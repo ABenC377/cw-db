b2afc6370fd71630d4c638bfaf38721da2b01d27
Alex Cockrean <alexander.cockrean@gmail.com>
2023-03-15 13:40:25 +0000

    finishing parser class

--- src/test/java/edu/uob/ParserTests.java
+++ src/test/java/edu/uob/ParserTests.java
@@ -1,4 +1,16 @@
 package edu.uob;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
 public class ParserTests {
+
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+
 }
